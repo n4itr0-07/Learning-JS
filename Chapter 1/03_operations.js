90570d813b7a31c028fb1256c60f4404220b3a41
Salik Seraj Naik <salikserajnaik@gmail.com>
2025-02-07 16:22:15 +0530

    Chapter 1 of JS learned Data Types & Operations

---
+++ Chapter 1/03_operations.js
@@ -0,0 +1,19 @@
+a = 20 
+b = 10
+
+// addition
+sum = a + b
+
+// subtraction
+difference = a - b
+
+// multiplication
+product = a * b
+
+// division
+quotient = a / b
+
+// modulus
+remainder = a % b
+
+console.log(sum)
