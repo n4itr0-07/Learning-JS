8248af1a6ae37a14e422d0f96f4e239814bced6e
Salik Seraj Naik <salikserajnaik@gmail.com>
2025-02-11 23:54:28 +0530

    Practice Questions

---
+++ Chapter 1/10_practice.js
@@ -0,0 +1,20 @@
+let name = "Mr. Robot";
+
+typeof name; // string
+name.length; // 8
+
+let firstName = "Mr.";
+firstName[0]; // M
+firstName.length; // 3
+
+firstName[firstName.length - 1]; // .
+
+let lastName = "Robot";
+lastName[0]; // R
+lastName.length; // 5
+
+let empty = "";
+empty.length; // 0
+
+let space = " ";
+space.length; // 1
