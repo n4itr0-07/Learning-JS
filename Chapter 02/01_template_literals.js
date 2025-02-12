cf81be622a31bb80f6c90572d0afa674f24a22c1
Salik Seraj Naik <salikserajnaik@gmail.com>
2025-02-12 22:55:52 +0530

    Template Literals

---
+++ Chapter 02/01_template_literals.js
@@ -0,0 +1,7 @@
+let a = 5;
+let b = 10;
+
+console.log(`Your pay ${a + b}, rupees`);
+//console.log("Price is", a+b, "rupees");
+
+// template literals are enclosed by the back-tick (` `)  (grave accent) character instead of double or single quotes. This allows you to embed expressions within the string by wrapping them in ${ }.
