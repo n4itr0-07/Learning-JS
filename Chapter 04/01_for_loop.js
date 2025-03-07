36c3a46acb1a223b2af5929459e7a8ab2ffcd6ac
Salik Seraj Naik <salikserajnaik@gmail.com>
2025-03-07 16:05:36 +0530

    For Loop In JS

---
+++ Chapter 04/01_for_loop.js
@@ -0,0 +1,27 @@
+/*
+                                            For Loop
+
+for (initialization; condition; increment) {
+     code block to be executed
+
+
+for (let i = 0; i < 5; i++) {
+    console.log(i);
+}  
+*/
+
+for (let i = 1; i <= 5; i++) {
+    console.log(i);
+}
+console.log('=====================');
+
+// Backwar loop
+
+for (let j = 7; j >= 1; j--) {
+    console.log(j);
+}
+
+// alert box using for loop
+for (let i = 1; i <= 5; i++) {
+    alert(i);
+}
