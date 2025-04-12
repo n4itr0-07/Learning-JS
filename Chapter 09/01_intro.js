ffe64437bcd6ea72d38825ff21b6836e91e34141
Salik Seraj Naik <salikserajnaik@gmail.com>
2025-04-12 18:23:49 +0530

    DOM Chapter 09 | 01_Introduction

---
+++ Chapter 09/01_intro.js
@@ -0,0 +1,11 @@
+/*
+                    DOM( Document Object Model)
+The DOM represents a document with a logical tree.
+It allows us to manipulate/change webpage content (HTML elements).
+The DOM is a programming interface for web documents.
+The DOM represents the structure of a document as a tree of objects.
+The DOM is a W3C (World Wide Web Consortium) standard.
+The DOM is language-independent, but it is most commonly used with JavaScript.
+The DOM is a platform-neutral interface that allows us to access and manipulate the content, structure, and style of a document.
+The DOM is a programming interface for web documents.
+*/
