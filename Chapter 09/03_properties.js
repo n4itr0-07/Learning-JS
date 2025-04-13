871cf01ccaf472dbe7c3582b732be789b1502a1a
Salik Seraj Naik <salikserajnaik@gmail.com>
2025-04-13 20:53:34 +0530

    DOM Chapter 09 | Properties Methods

---
+++ Chapter 09/03_properties.js
@@ -0,0 +1,22 @@
+/*
+
+TODO: Using Properties & Methods
+
+inner Text
+    Shows the visible text contained in a node
+
+
+textContent
+    Shows all the full text
+
+
+inner HTML
+    Shows the full markup
+
+
+*/
+
+parsa.innerHTML = "<h1>hello</h1>";
+parsa.innerText = "hello world"; // Shows the visible text contained in a node
+parsa.textContent = "hello world"; // Shows all the full text
+
