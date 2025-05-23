f228fc12b895ad7cc29a9950d240a7ee60c8b408
Salik Seraj Naik <salikserajnaik@gmail.com>
2025-05-23 23:57:30 +0530

    Chapter 01 | Practice

---
+++ Chapter 01/11_practice.js
@@ -0,0 +1,14 @@
+// Chapter 1 Practice of JS
+
+let str = "Hello World!";
+let str2 = "Hello World!";
+
+let num = 10;
+
+console.log(str == str2); // true
+console.log(str === str2); // true
+console.log(str != str2); // false
+console.log(str !== str2); // false
+console.log(str > str2); // false
+
+console.log(`${str} == ${str2}: ${str == str2}`); // true
